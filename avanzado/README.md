a508d383a25132631ce34f468e9a40f8722fc0c6
alex2022-lab <111999591+alex2022-lab@users.noreply.github.com>
2024-04-24 16:22:34 -0400

    Taller de Vue avanzado realizado
    A continuación hago entrega del taller de vue avanzado de la sesión 14 que está realizado, debo destacar que lo que me faltó por realizar es la acción de modificar usuario

---
+++ avanzado/README.md
@@ -0,0 +1,32 @@
+# avanzado
+
+El codigo cuenta con una interfaz grafica con un formulario para registro de usuarios y una lista usando Vue.js
+
+En la etiqueta Template se encuentra todo lo relacionado a HTML como el input, los botones con sus acciones y la lista de usuarios
+
+En la etiqueta script se encuentran definidos los métodos de acciones de cada botón junto con las importaciones de la reactividad y los componentes, la mayoría de importaciones y métodos se encuentran en el archivo Formulario_registro.vue
+
+En la etiqueta Style se encuentran definidos los estilos del formulario de registro y lista de usuarios
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ avanzado/src/components/Formulario_registro.vue
@@ -0,0 +1,51 @@
+<template>
+
+   <div class="form">
+    <h1>Registro</h1>
+        <ul>
+            <li v-for="usuario in usuarios" :key="usuario.id">{{ usuario.nombre }}</li>
+        </ul>
+        <input v-model="nuevoUsuario"  @keyup.enter="añadirUsuario" placeholder="Nombre y apellido" required>
+        <button @click="añadirUsuario">Agregar usuario</button>
+        <button @click="borrarUsuario">BorrarUsuario</button>
+    </div>
+    
+</template>
+
+<script>
+import {reactive} from 'vue'
+
+export default {
+  data() {
+    return {
+      usuarios: [
+      ],
+      nuevoUsuario: ''
+
+    };
+  },
+  methods: {
+    añadirUsuario() {
+        if (this.nuevoUsuario.trim() !== '') {
+            // reactividad
+        const nuevaId = reactive(this.usuarios.length + 1);
+        this.usuarios.push({ id: nuevaId, nombre: this.nuevoUsuario });
+        this.nuevoUsuario = '';
+    }
+  },
+  borrarUsuario(index) {
+    this.usuarios.splice(index, 1);
+  }
+}
+};
+</script>
+
+<style>
+.form{
+    border: solid;
+    padding: 10px;
+    border-radius: 15px;
+    background: aquamarine;
+}
+
+</style>
